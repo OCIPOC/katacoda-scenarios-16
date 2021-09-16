6db449a60f0a5530e08c0ab704a4fe351700a148
Bootcamp AI <39755290+AlexPaolo@users.noreply.github.com>
2021-09-16 12:49:27 -0500

    Postgres 1

--- postgres1/set-env.sh
+++ postgres1/set-env.sh
@@ -11,8 +11,8 @@ until PGPASSWORD="password" psql -h localhost -U groot postgres -c '\l' &> /dev/
   sleep 1
 done
 
-echo 'Set environment...'
-curl PGPASSWORD="password" psql -h localhost -U groot nyc
+#echo 'Loading data...'
+#curl http://s3.cleverelephant.ca/nyc_data.sql.gz | gzip -dc | PGPASSWORD="password" psql -h localhost -U groot nyc
 
 echo 'Finished and ready to go.'
 clear
